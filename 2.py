f6ef0813185128df7aeb89535a63ab79b15e1766
Ganeesha Sandeepani <ganeesha@iq.lk>
2020-08-03 02:28:16 +0530

    q3 commit

--- 2.py
+++ 2.py
@@ -15,13 +15,11 @@ import keras
 X_train = X_train.reshape((X_train.shape[0], 28, 28, 1)).astype('float32')
 X_test = X_test.reshape((X_test.shape[0], 28, 28, 1)).astype('float32')
 
-# normalize inputs from 0-255 to 0-1
 X_train = X_train / 255
 X_test = X_test / 255
 
 #generate  noice data
-def noice_data(noice_factor):
-    
+def noice_data(noice_factor):    
     
     X_train_noisy=X_train + noice_factor*np.random.normal(loc=0.0, scale = 1.0, size=X_train.shape)
     X_test_noisy=X_test + noice_factor*np.random.normal(loc=0.0, scale = 1.0, size=X_test.shape)
@@ -30,7 +28,6 @@ def noice_data(noice_factor):
     X_test_noisy = np.clip(X_test_noisy,0.,1.) 
     return X_train_noisy,X_test_noisy
 
-# one hot encode outputs
 y_train = np_utils.to_categorical(y_train)
 y_test = np_utils.to_categorical(y_test)
 num_classes = y_test.shape[1]
@@ -51,13 +48,12 @@ def create_model():
  
 
 callback = EarlyStopping(monitor='val_loss', patience=3)
-#model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=10, batch_size=200)
 for noice_fact in np.arange(0,1.050,0.050):
     print(noice_fact)
     X_train_noisy,X_test_noisy = noice_data(noice_fact)
     
     model = create_model()    
-    model.fit(X_train_noisy, y_train, validation_split=0.1, epochs=1, batch_size=200,callbacks=[callback])
+    model.fit(X_train_noisy, y_train, validation_split=0.1, epochs=20, batch_size=200,callbacks=[callback], verbose=0)
     
     scores = model.evaluate(X_test_noisy, y_test, verbose=0)
     print("Noice factor: %.2f%%, Accuracy: %.2f%%" % (noice_fact,scores[1]*100))
---
+++ 3.py
@@ -0,0 +1,142 @@
+import keras
+from matplotlib import pyplot as plt
+import numpy as np
+from keras.datasets import mnist
+from keras.models import Model
+from keras.optimizers import RMSprop
+from keras.layers import Input,Dense,Flatten,Dropout,merge,Reshape,Conv2D,MaxPooling2D,UpSampling2D,Conv2DTranspose
+from keras.layers.normalization import BatchNormalization
+from keras.models import Model,Sequential
+from keras.callbacks import ModelCheckpoint
+from keras.optimizers import Adadelta, RMSprop,SGD,Adam
+from keras import regularizers
+from keras import backend as K
+from keras.utils import to_categorical
+from keras.utils import np_utils
+
+
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+
+X_train = X_train.reshape((X_train.shape[0], 28, 28, 1)).astype('float32')
+X_test = X_test.reshape((X_test.shape[0], 28, 28, 1)).astype('float32')
+
+X_train = X_train / 255
+X_test = X_test / 255
+
+noice_factor  = 0.25
+X_train_noisy=X_train + noice_factor*np.random.normal(loc=0.0, scale = 1.0, size=X_train.shape)
+X_test_noisy=X_test + noice_factor*np.random.normal(loc=0.0, scale = 1.0, size=X_test.shape)
+
+X_train_noisy = np.clip(X_train_noisy,0.,1.)
+X_test_noisy = np.clip(X_test_noisy,0.,1.) 
+
+y_train = np_utils.to_categorical(y_train)
+y_test = np_utils.to_categorical(y_test)
+
+def encoder(input_img):
+
+    conv1 = Conv2D(32, (3, 3), activation='relu', padding='same')(input_img) #28 x 28 x 32
+    conv1 = BatchNormalization()(conv1)
+    conv1 = Conv2D(32, (3, 3), activation='relu', padding='same')(conv1)
+    conv1 = BatchNormalization()(conv1)
+    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1) #14 x 14 x 32
+    conv2 = Conv2D(64, (3, 3), activation='relu', padding='same')(pool1) #14 x 14 x 64
+    conv2 = BatchNormalization()(conv2)
+    conv2 = Conv2D(64, (3, 3), activation='relu', padding='same')(conv2)
+    conv2 = BatchNormalization()(conv2)
+    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2) #7 x 7 x 64
+    conv3 = Conv2D(128, (3, 3), activation='relu', padding='same')(pool2) #7 x 7 x 128 (small and thick)
+    conv3 = BatchNormalization()(conv3)
+    conv3 = Conv2D(128, (3, 3), activation='relu', padding='same')(conv3)
+    conv3 = BatchNormalization()(conv3)
+    conv4 = Conv2D(256, (3, 3), activation='relu', padding='same')(conv3) #7 x 7 x 256 (small and thick)
+    conv4 = BatchNormalization()(conv4)
+    conv4 = Conv2D(256, (3, 3), activation='relu', padding='same')(conv4)
+    conv4 = BatchNormalization()(conv4)
+    return conv4
+
+def decoder(conv4):    
+
+    conv5 = Conv2D(128, (3, 3), activation='relu', padding='same')(conv4) #7 x 7 x 128
+    conv5 = BatchNormalization()(conv5)
+    conv5 = Conv2D(128, (3, 3), activation='relu', padding='same')(conv5)
+    conv5 = BatchNormalization()(conv5)
+    conv6 = Conv2D(64, (3, 3), activation='relu', padding='same')(conv5) #7 x 7 x 64
+    conv6 = BatchNormalization()(conv6)
+    conv6 = Conv2D(64, (3, 3), activation='relu', padding='same')(conv6)
+    conv6 = BatchNormalization()(conv6)
+    up1 = UpSampling2D((2,2))(conv6) #14 x 14 x 64
+    conv7 = Conv2D(32, (3, 3), activation='relu', padding='same')(up1) # 14 x 14 x 32
+    conv7 = BatchNormalization()(conv7)
+    conv7 = Conv2D(32, (3, 3), activation='relu', padding='same')(conv7)
+    conv7 = BatchNormalization()(conv7)
+    up2 = UpSampling2D((2,2))(conv7) # 28 x 28 x 32
+    decoded = Conv2D(1, (3, 3), activation='sigmoid', padding='same')(up2) # 28 x 28 x 1
+    return decoded
+input_img = Input(shape=(28, 28, 1))  # adapt this if using `channels_first` image data format
+
+autoencoder = Model(input_img, decoder(encoder(input_img)))
+autoencoder.compile(loss='mean_squared_error', optimizer = RMSprop())
+
+batch_size = 256
+epochs = 20
+num_classes = 10
+autoencoder_train = autoencoder.fit(X_train, X_train, batch_size=batch_size,epochs=epochs,verbose=1,validation_split=0.2)
+
+
+def fc(enco):
+    flat = Flatten()(enco)
+    den = Dense(128, activation='relu')(flat)
+    out = Dense(num_classes, activation='softmax')(den)
+    return out
+encode = encoder(input_img)
+full_model = Model(input_img,fc(encode))
+
+for l1,l2 in zip(full_model.layers[:19],autoencoder.layers[0:19]):
+    l1.set_weights(l2.get_weights())
+    
+#autoencoder.get_weights()[0][1]
+#full_model.get_weights()[0][1]
+    
+for layer in full_model.layers[0:19]:
+    layer.trainable = False
+full_model.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimizers.Adam(),metrics=['accuracy'])
+#full_model.summary()
+classify_train = full_model.fit(X_train, y_train, batch_size=64,epochs=100,verbose=1,validation_split=0.2)
+
+full_model.save_weights('autoencoder_classification.h5')
+for layer in full_model.layers[0:19]:
+    layer.trainable = True
+full_model.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimizers.Adam(),metrics=['accuracy'])
+
+classify_train = full_model.fit(X_train, y_train, batch_size=64,epochs=100,verbose=1,validation_split=0.2)
+
+full_model.save_weights('classification_complete.h5')
+
+accuracy = classify_train.history['acc']
+val_accuracy = classify_train.history['val_acc']
+loss = classify_train.history['loss']
+val_loss = classify_train.history['val_loss']
+epochs = range(len(accuracy))
+plt.plot(epochs, accuracy, 'bo', label='Training accuracy')
+plt.plot(epochs, val_accuracy, 'b', label='Validation accuracy')
+plt.title('Training and validation accuracy')
+plt.legend()
+plt.figure()
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+plt.show()
+
+test_eval = full_model.evaluate(X_test, y_test, verbose=0)
+
+print('Test loss:', test_eval[0])
+print('Test accuracy:', test_eval[1])
+
+predicted_classes = full_model.predict(X_test)
+
+predicted_classes = np.argmax(np.round(predicted_classes),axis=1)
+
+
+
